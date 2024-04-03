446de352829da5127681d52aef63bfc4ea188330
Volvo <Volvo@DESKTOP-8TV1PCA>
2024-04-03 10:47:54 +0300

    Добавьте файлы проекта.

---
+++ Helper/FindPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.Helper
+{
+    internal class FindPerson
+    {
+        int id;
+
+        public FindPerson(int id)
+        {
+            this.id = id;
+        }
+
+        public bool PersonPredicate(Person obj)
+        {
+            return obj.Id == id;
+        }
+    }
+}
